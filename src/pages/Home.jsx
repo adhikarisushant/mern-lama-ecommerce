315ba11e11a84521a217a5195f52d45789d7d7a4
adhikarisushant <sushant.adhikari.619@gmail.com>
2021-12-29 12:21:27 +0545

    Ecommerce App Navbar Design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Announcement from '../components/Announcement'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
     return (
         <div>
-            HOMEPAGE
+            <Announcement />
+            <Navbar />
         </div>
     )
 }
